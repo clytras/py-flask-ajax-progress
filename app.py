f89bd9aff00c3c255237175e26864436441c247c
Christos Lytras <christos.lytras@gmail.com>
2020-04-22 14:17:31 +0300

    add line feeds to responses

--- app.py
+++ app.py
@@ -14,20 +14,20 @@ def compute_image():
         progress = 0
 
         # Start by sending total JSON value to the client stream
-        yield "{\"total\": %d}" % huge_number
+        yield "{\"total\": %d}\n" % huge_number
 
         for i in range(huge_number):
             # If there is a 100 step progress, send progress JSON value to the client stream
             if progress >= 100:
                 progress = 0
-                yield "{\"progress\": %d}" % (i)
+                yield "{\"progress\": %d}\n" % (i)
             # Else increment progress by 1 and sleep a bit
             else:
                 progress = progress + 1
                 time.sleep(0.001)
         
         # Send the generated image location to the client when we're done
-        yield "{\"img_url\": \"/some/generated/image.png\"}"
+        yield "{\"img_url\": \"/some/generated/image.png\"}\n"
     
     # Return the stream context with out generator function
     return Response(stream_with_context(generate()), mimetype='text/event-stream')
